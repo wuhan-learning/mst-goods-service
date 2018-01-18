b87278463f02bedac9c3284aaa3a0c17a56da6f1
Yvette14 <libra1014tyb@163.com>
2018-01-18 22:17:57 +0800

    [Yibing] #N/A Enable feign client and discovery client.

--- src/main/java/com/thoughtworks/mstorderservice/MstGoodsServiceApplication.java
+++ src/main/java/com/thoughtworks/mstorderservice/MstGoodsServiceApplication.java
@@ -2,8 +2,12 @@ package com.thoughtworks.mstorderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class MstGoodsServiceApplication {
 
 	public static void main(String[] args) {
